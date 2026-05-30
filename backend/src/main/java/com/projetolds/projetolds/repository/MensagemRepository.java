ef9098c6e8ca5ce11892962581c43317b62bb658
felipemths12 <felipe.matheussms@gmail.com>
2026-05-30 20:12:37 -0300

    feat: buscar mensagens de um atendimento

--- backend/src/main/java/com/projetolds/projetolds/repository/MensagemRepository.java
+++ backend/src/main/java/com/projetolds/projetolds/repository/MensagemRepository.java
@@ -1,7 +1,11 @@
 package com.projetolds.projetolds.repository;
 
+import com.projetolds.projetolds.model.Atendimento;
 import com.projetolds.projetolds.model.Mensagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MensagemRepository extends JpaRepository<Mensagem, Long> {
+    List<Mensagem> findByAtendimento(Atendimento atendimento);
 }
