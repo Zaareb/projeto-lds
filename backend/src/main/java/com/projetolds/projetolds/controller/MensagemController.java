e5841fbdea63f4e4e69c2e23d9829e6a3e818501
felipemths12 <felipe.matheussms@gmail.com>
2026-05-30 20:40:57 -0300

    feat: mostrar todo o chat

--- backend/src/main/java/com/projetolds/projetolds/controller/MensagemController.java
+++ backend/src/main/java/com/projetolds/projetolds/controller/MensagemController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/mensagens")
 @CrossOrigin(origins = "http://localhost:5173")
@@ -21,5 +23,11 @@ public class MensagemController {
     public ResponseEntity<Mensagem> enviarMensagens(@RequestBody @Valid EnviarMensagemDTO enviarMensagemDTO) {
         Mensagem mensagem = mensagemService.enviarMensagem(enviarMensagemDTO);
         return ResponseEntity.ok(mensagem);
+    }
+
+    @GetMapping("/protocolo/{numero_protocolo}")
+    public ResponseEntity<List<Mensagem>> listarChat(@PathVariable Long numero_protocolo) {
+        List<Mensagem> historico = mensagemService.listarHistoricoDeMensagens(numero_protocolo);
+        return ResponseEntity.ok(historico);
     }
 }
